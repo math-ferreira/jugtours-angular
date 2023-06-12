4d4f873d71e95a04c7620c027e9af311a4368446
math-ferreira <mat.s.ferreira@gmail.com>
2023-06-11 21:05:54 -0300

    adding feature to add and edit a group

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GroupListComponent } from './group-list/group-list.component';
+import { GroupEditComponent } from './group-edit/group-edit.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'groups',
     component: GroupListComponent
+  },
+  {
+    path: 'group/:id',
+    component: GroupEditComponent
   }
 ];
 
